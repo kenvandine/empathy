37a9b435efdb27dad389763a12b9bad185b700d5
Xavier Claessens <xclaesse@gmail.com>
2007-09-27 08:13:51 +0000

    No need to init gtk, GMainLoop is enough and makes easier to run valgrind.
    2007-09-27  Xavier Claessens  <xclaesse@gmail.com>
    
    	* tests/contact-manager.c:
    	* tests/Makefile.am: No need to init gtk, GMainLoop is enough and makes
    	easier to run valgrind.
    
    
    svn path=/trunk/; revision=324

--- tests/contact-manager.c
+++ tests/contact-manager.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 
-#include <gtk/gtk.h>
+#include <glib.h>
 #include <libempathy/empathy-contact-manager.h>
 
 static gboolean
-time_out (gpointer data)
+time_out (gpointer main_loop)
 {
-	gtk_main_quit ();
+	g_main_loop_quit (main_loop);
 
 	return FALSE;
 }
@@ -15,16 +15,20 @@ int
 main (int argc, char **argv)
 {
 	EmpathyContactManager *manager;
+	GMainLoop             *main_loop;
 
-	gtk_init (&argc, &argv);
+	g_type_init ();
 
+	main_loop = g_main_loop_new (NULL, FALSE);
 	manager = empathy_contact_manager_new ();
 	
-	g_timeout_add (5000, time_out, NULL);
+	g_timeout_add (5000, time_out, main_loop);
 
-	gtk_main ();
+	g_main_loop_run (main_loop);
 	
 	g_object_unref (manager);
+	g_main_loop_unref (main_loop);
 
 	return EXIT_SUCCESS;
 }
+
