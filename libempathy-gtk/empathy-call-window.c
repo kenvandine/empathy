caec259128182461e7454090cb7d88d7919483b2
Xavier Claessens <xclaesse@src.gnome.org>
2007-12-29 15:49:36 +0000

    Set call window title to contact name
    svn path=/trunk/; revision=513

--- libempathy-gtk/empathy-call-window.c
+++ libempathy-gtk/empathy-call-window.c
@@ -104,6 +104,20 @@ call_window_capabilities_notify_cb (EmpathyContact    *contact,
 }
 
 static void
+call_window_name_notify_cb (EmpathyContact    *contact,
+			    GParamSpec        *param,
+			    EmpathyCallWindow *window)
+{
+	const gchar *name;
+	gchar       *title;
+
+	name = empathy_contact_get_name (contact);
+	title = g_strdup_printf (_("Call from %s"), name);
+	gtk_window_set_title (GTK_WINDOW (window->window), title);
+	g_free (title);
+}
+
+static void
 call_window_status_notify_cb (EmpathyTpCall     *call,
 			      GParamSpec        *param,
 			      EmpathyCallWindow *window)
@@ -124,7 +138,11 @@ call_window_status_notify_cb (EmpathyTpCall     *call,
 			g_signal_connect (contact, "notify::capabilities",
 					  G_CALLBACK (call_window_capabilities_notify_cb),
 					  window);
+			g_signal_connect (contact, "notify::name",
+					  G_CALLBACK (call_window_name_notify_cb),
+					  window);
 			call_window_capabilities_notify_cb (contact, NULL, window);
+			call_window_name_notify_cb (contact, NULL, window);
 		}
 	}
 
