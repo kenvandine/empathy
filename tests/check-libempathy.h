2c4f7c6b6549f8b95845f1838dd0dc710c9e8304
Xavier Claessens <xclaesse@src.gnome.org>
2007-12-29 15:49:28 +0000

    Add "check" framework support. Fixes bug #505622 (Guillaume Desmottes).
    svn path=/trunk/; revision=509

---
+++ tests/check-libempathy.h
@@ -0,0 +1,6 @@
+#ifndef __CHECK_LIBEMPATHY__
+#define __CHECK_LIBEMPATHY__
+
+TCase * make_empathy_utils_tcase (void);
+
+#endif /* #ifndef __CHECK_LIBEMPATHY__ */
---
+++ tests/check-main.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <glib-object.h>
+
+#include <check.h>
+
+#include "check-helpers.h"
+#include "check-libempathy.h"
+
+#include "config.h"
+
+static Suite *
+make_libempathy_suite (void)
+{
+    Suite *s = suite_create ("libempathy");
+
+    suite_add_tcase (s, make_empathy_utils_tcase ());
+
+    return s;
+}
+
+int
+main (void)
+{
+    int number_failed = 0;
+    Suite *s;
+    SRunner *sr;
+
+    check_helpers_init ();
+    g_type_init ();
+
+    s = make_libempathy_suite ();
+    sr = srunner_create (s);
+    srunner_run_all (sr, CK_NORMAL);
+    number_failed += srunner_ntests_failed (sr);
+    srunner_free (sr);
+
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
