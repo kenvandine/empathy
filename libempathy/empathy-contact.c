fb6eb54dc4b34fa30f67bf59288dc0d2247c78bf
Xavier Claessens <xclaesse@src.gnome.org>
2008-02-17 20:56:54 +0000

    EmpathyContact: Set G_PARAM_CONSTRUCT on the capabilities property. (Sjoerd)
    svn path=/trunk/; revision=646

--- libempathy/empathy-contact.c
+++ libempathy/empathy-contact.c
@@ -158,7 +158,7 @@ empathy_contact_class_init (EmpathyContactClass *class)
 							     "Capabilities of the contact",
 							     EMPATHY_TYPE_CAPABILITIES,
 							     EMPATHY_CAPABILITIES_UNKNOWN,
-							     G_PARAM_READWRITE));
+							     G_PARAM_CONSTRUCT | G_PARAM_READWRITE));
 
 	g_object_class_install_property (object_class,
 					 PROP_IS_USER,
