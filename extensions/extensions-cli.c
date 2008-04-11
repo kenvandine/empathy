c99bbf0fdfd30fd2e71125cb105d2c1339cffe4d
Xavier Claessens <xclaesse@src.gnome.org>
2008-04-11 13:11:27 +0000

    Update extensions dir based on the example in tp-glib
    svn path=/trunk/; revision=917

--- extensions/extensions-cli.c
+++ extensions/extensions-cli.c
@@ -9,17 +9,23 @@ static void _emp_ext_register_dbus_glib_marshallers (void);
 #include "_gen/cli-misc-body.h"
 #include "_gen/register-dbus-glib-marshallers-body.h"
 
+static gpointer
+emp_cli_once (gpointer data)
+{
+  _emp_ext_register_dbus_glib_marshallers ();
+
+  tp_proxy_init_known_interfaces ();
+
+  tp_proxy_or_subclass_hook_on_interface_add (TP_TYPE_PROXY,
+      emp_cli_misc_add_signals);
+
+  return NULL;
+}
+
 void
 emp_cli_init (void)
 {
-  static gboolean initialized = FALSE;
+  static GOnce once = G_ONCE_INIT;
 
-  if (!initialized)
-    {
-      _emp_ext_register_dbus_glib_marshallers ();
-
-      tp_proxy_or_subclass_hook_on_interface_add (TP_TYPE_PROXY,
-          emp_cli_misc_add_signals);
-      initialized = TRUE;
-    }
+  g_once (&once, emp_cli_once, NULL);
 }
