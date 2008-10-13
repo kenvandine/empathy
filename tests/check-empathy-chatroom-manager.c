566da7c05622e1463ef84967a2b81c295c113812
Xavier Claessens <xclaesse@src.gnome.org>
2008-10-13 07:54:15 +0000

    load the chatrooms sample XML file
    svn path=/trunk/; revision=1552

--- tests/check-empathy-chatroom-manager.c
+++ tests/check-empathy-chatroom-manager.c
@@ -15,14 +15,33 @@
 START_TEST (test_empathy_chatroom_manager_new)
 {
   EmpathyChatroomManager *mgr;
+  gchar *cmd;
   gchar *file;
+  McProfile *profile;
+  McAccount *account;
+
+  profile = mc_profile_lookup ("test");
+  account = mc_account_create (profile);
 
   copy_xml_file (CHATROOM_SAMPLE, CHATROOM_FILE);
-  file = get_xml_file (CHATROOM_FILE);
+
+  file = get_user_xml_file (CHATROOM_FILE);
+  /* change the chatrooms XML file to use the account we just created */
+  cmd = g_strdup_printf ("sed -i 's/CHANGE_ME/%s/' %s",
+      mc_account_get_unique_name (account), file);
+  system (cmd);
+  g_free (cmd);
+
   mgr = empathy_chatroom_manager_new (file);
+
+  fail_if (empathy_chatroom_manager_get_count (mgr, account) != 2);
 
   g_free (file);
   g_object_unref (mgr);
+  g_object_unref (profile);
+  remove_account_from_gconf (account);
+  mc_account_delete (account);
+  g_object_unref (account);
 }
 END_TEST
 
