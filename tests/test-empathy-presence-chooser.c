983528dcb684f3f8339d13d5beb52d2b1e4ca3a0
Davyd Madeley <davyd@madeley.id.au>
2009-04-10 16:54:07 +0000

    Fix typo in my email address
    From: Davyd Madeley <davyd@madeley.id.au>
    
    svn path=/trunk/; revision=2784

--- tests/test-empathy-presence-chooser.c
+++ tests/test-empathy-presence-chooser.c
@@ -17,7 +17,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * Authors: Davyd Madeley <davvyd.madeley@collabora.co.uk>
+ * Authors: Davyd Madeley <davyd.madeley@collabora.co.uk>
  */
 
 #include <config.h>
