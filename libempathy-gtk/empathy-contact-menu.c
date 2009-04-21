27ba4ebcae6321cb8d57c90f25351376f50cd99c
Frederic Peters <fpeters@0d.be>
2009-04-21 16:37:22 +0200

    Add context string for translation (#575442)

--- libempathy-gtk/empathy-contact-menu.c
+++ libempathy-gtk/empathy-contact-menu.c
@@ -158,7 +158,7 @@ empathy_contact_call_menu_item_new (EmpathyContact *contact)
 
 	g_return_val_if_fail (EMPATHY_IS_CONTACT (contact), NULL);
 
-	item = gtk_image_menu_item_new_with_mnemonic (_("_Call"));
+	item = gtk_image_menu_item_new_with_mnemonic (C_("menu item", "_Call"));
 	image = gtk_image_new_from_icon_name (EMPATHY_IMAGE_VOIP,
 					      GTK_ICON_SIZE_MENU);
 	gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (item), image);
