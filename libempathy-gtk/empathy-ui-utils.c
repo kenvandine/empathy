d3fc1349507da7d0bb6538a8e66fa3116bb6eec9
Cosimo Cecchi <cosimoc@gnome.org>
2009-05-06 15:04:12 +0200

    Add a new preference in the UI

--- libempathy-gtk/empathy-ui-utils.c
+++ libempathy-gtk/empathy-ui-utils.c
@@ -1411,14 +1411,21 @@ file_manager_send_file_response_cb (GtkDialog      *widget,
 	GFile *file;
 	gchar *uri;
 	GtkRecentManager *manager;
+	gboolean use_hash;
 
 	if (response_id == GTK_RESPONSE_OK) {
 		file = gtk_file_chooser_get_file (GTK_FILE_CHOOSER (widget));
 		uri = g_file_get_uri (file);
 
+		use_hash = empathy_conf_get_bool
+			(empathy_conf_get (),
+			 EMPATHY_PREFS_FILE_TRANSFER_USE_HASH,
+			 &use_hash);
+
 		factory = empathy_ft_factory_dup_singleton ();
 
-		empathy_ft_factory_new_transfer_outgoing (factory, contact, file);
+		empathy_ft_factory_new_transfer_outgoing (factory, contact,
+		                                          file, use_hash);
 
 		manager = gtk_recent_manager_get_default ();
 		gtk_recent_manager_add_item (manager, uri);
