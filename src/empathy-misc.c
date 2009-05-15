5ff364c2645ecd4aed79340455f1247fe0f37838
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-05-15 15:34:12 +0100

    empathy-misc: remove trailing spaces

--- src/empathy-misc.c
+++ src/empathy-misc.c
@@ -16,7 +16,7 @@
  * License along with this program; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- * 
+ *
  * Author: Cosimo Cecchi <cosimo.cecchi@collabora.co.uk>
  *
  */
