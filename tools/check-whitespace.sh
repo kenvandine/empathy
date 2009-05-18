6ef0624346203591e00270f4f595c5f648278547
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-05-18 10:03:21 +0100

    disable tab coding style check for now

--- tools/check-whitespace.sh
+++ tools/check-whitespace.sh
@@ -8,10 +8,11 @@ then
   fail=1
 fi
 
-if grep -n '	' "$@"
-then
-  echo "^^^ The above files contain tabs"
-  fail=1
-fi
+# TODO: enable tab checking once all Empathy switched to TP coding style
+#if grep -n '	' "$@"
+#then
+#  echo "^^^ The above files contain tabs"
+#  fail=1
+#fi
 
 exit $fail
