35674b38a744e4024911f9e38c231ea687fc16d3
Pierre-Luc Beaudoin <pierre-luc@pierlux.com>
2009-05-20 13:21:56 -0400

    Undo changes to EmpathyContactDialogs

--- libempathy-gtk/empathy-contact-dialogs.c
+++ libempathy-gtk/empathy-contact-dialogs.c
@@ -247,11 +247,11 @@ empathy_contact_edit_dialog_show (EmpathyContact *contact,
 	gtk_window_set_default (GTK_WINDOW (dialog), button);
 	gtk_widget_show (button);
 
+	/* Contact info widget */
 	contact_widget = empathy_contact_widget_new (contact,
 		EMPATHY_CONTACT_WIDGET_EDIT_ALIAS |
 		EMPATHY_CONTACT_WIDGET_EDIT_GROUPS);
 	gtk_container_set_border_width (GTK_CONTAINER (contact_widget), 8);
-
 	gtk_box_pack_start (GTK_BOX (GTK_DIALOG (dialog)->vbox),
 			    contact_widget,
 			    TRUE, TRUE, 0);
