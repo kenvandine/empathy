aa9603b62dd1893eb3fdcd357397ba20c733da43
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2009-06-24 11:25:52 +0100

    check-whitespace.sh: check trailing tabs

--- tools/check-whitespace.sh
+++ tools/check-whitespace.sh
@@ -8,6 +8,12 @@ then
   fail=1
 fi
 
+if grep -n '	$' "$@"
+then
+  echo "^^^ The above files contain unwanted trailing tabs"
+  fail=1
+fi
+
 # TODO: enable tab checking once all Empathy switched to TP coding style
 #if grep -n '	' "$@"
 #then
